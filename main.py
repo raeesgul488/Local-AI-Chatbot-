d7a169b211299c225dbd416d61f4e153066dc7c3
Raees Ahmad <raeesahm65@gmail.com>
2026-09-23 11:17:49 +0500

    Remove redundant main.py file

--- main.py
+++
@@ -1,8 +0,0 @@
-from ollama import chat
-prompt = input("Enter prompt")
-
-response = chat(
-    model='gemma3:1b',
-    messages=[{'role': 'user', 'content':prompt}],
-)
-print(response.message.content)
